752a04af36dae6a7fca67d204eb408d109cc3d00
josefina Inurrategui <josefina.inurrategui@gmail.com>
2020-06-24 12:47:51 -0300

    participants endpoint

---
+++ participants/participants.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const dbConf = require('../Database/databaseConf.js');        
+const DataBase = new Sequelize(`${dbConf.dialect}://${dbConf.user}:${dbConf.password}@${dbConf.host}:${dbConf.port}/${dbConf.db_name}`);
+
+module.exports ={ 
+    newParticipant: (req,res) => {
+        DataBase.query(
+            'INSERT INTO participants (complete_name, email, participant_desc) VALUES (:complete_name, :email, :participant_desc)',{
+                replacements: req.body
+            }).then(result => console.log(result) || res.status(200).json('New participant created'))
+              .catch(error => console.log(error) || res.status(400).send('Invalid data'))
+    }
+
+}
